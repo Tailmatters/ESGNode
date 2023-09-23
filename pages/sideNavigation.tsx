f2e8a31faec82b09de645840b9af4a0c73c3cf7d
Tailmatters <vithal@ctrlspend.com>
2023-09-23 20:54:22 +0530

    borders on side and top

---
+++ pages/sideNavigation.tsx
@@ -0,0 +1,4 @@
+const sideNavigation = () => {
+    return <div className="flex border-2 border-blue-500">Subscription Info</div>
+};
+export default sideNavigation;
---
+++ pages/topNavigation.tsx
@@ -0,0 +1,4 @@
+const topNavigation = () => {
+    return <div className="border-2 border-orange-500">ESG Info</div>
+};
+export default topNavigation;
