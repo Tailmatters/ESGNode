6c207b95a69b9f0230b2f4bf6fcc7e0f8ffdf502
Tailmatters <vithal@ctrlspend.com>
2024-08-15 18:06:31 +0530

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ export default function Web() {
             ESG Node - Your Partner For Driving Triple Bottomline
             </h1>
             <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
-              Trying to build a world class ESG Product for the benefit of Indian listed companies. Contact vithal@ctrlspend.com for more details.
+              Trying to build a world class ESG software for the benefit of Indian listed companies. Contact vithal@ctrlspend.com for more details.
             </p>
             <Button href="https://github.com/Blazity/next-enterprise" className="mr-3">
               Learn More
